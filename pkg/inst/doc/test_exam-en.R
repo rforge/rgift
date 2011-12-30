c3e21c132cc9d98ff2748ea5d8eb323991413191
virgil <virgil@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-30 23:27:05 +0000

    Minor corrections.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgift@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/doc/test_exam-en.R
+++ pkg/inst/doc/test_exam-en.R
@@ -9,13 +9,19 @@ library(RGIFT)
 #garbage in the output  file
 #
 
+#Redefine function to set default values. This has been done to
+#simplify the modifications in the original file
+pregunta<-function(qtxt, atxt){GIFTMC(atxt, atxt, wwrong="-33.333")}
+
+
 #Set file where the questions are saved
 sink("test_exam.txt")
 
 #
 #Practical 1: Intro to R
 #
-cat("\n\n$CATEGORY: Ex_Pr1\n\n")
+#cat("\n\n$CATEGORY: Ex_Pr1\n\n")
+GIFTcategory("Ex_Pr1")
 
 for(i in 1:5)#5 different data sets 
 {
@@ -57,15 +63,15 @@ for(i in 1:5)#5 different data sets
 
 
 #Some questions about specific commands
-pregunta("I order to know the length of a vecot in R, you would use:",
+pregunta("In order to know the length of a vector in R, you will use:",
    c("length()", "long()", "None of the above")
 )
 
-pregunta("To make a bar plot you would use:",
+pregunta("To make a bar plot you will use:",
    c("barplot()", "plot()", "None of the above")
 )
 
-pregunta("To make a histogram you would use:",
+pregunta("To make a histogram you will use:",
    c("None of the above","histo()", "h()")
 )
 
@@ -73,15 +79,15 @@ pregunta("The command to compute the square root of 13 is:",
    c("sqrt(13)", "sd(13)", "str(13)")
 )
 
-pregunta("To compute factorial of 5, you would use:",
+pregunta("To compute factorial of 5, you will use:",
    c("factorial(5)", "facto(5)", "f(5)")
 )
 
-pregunta("To make a sequence of 10 values between 0 and 10, you would write:",
+pregunta("To make a sequence of 10 values between 0 and 10, you will write:",
    c("seq(0,5,length\\=10)", "0:5", "sequ(0,5)" )
 )
 
-pregunta("To make a pie chart, what function would you use?:",
+pregunta("To make a pie chart, what function will you use?:",
    c("None of the above", "sector()", "tarta()")
 )
 
@@ -92,9 +98,11 @@ pregunta("A pie chart is done for ",
 
 
 #
-#Practical 2:
+#Practical 2: Simulation with R (I)
 #
-cat("\n\n$CATEGORY: Ex_Pr2\n\n")
+#cat("\n\n$CATEGORY: Ex_Pr2\n\n")
+GIFTcategory("Ex_Pr2")
+
 
 for(i in 1:5)#5 different vectors of data
 {
@@ -107,51 +115,51 @@ for(i in 1:5)#5 different vectors of data
    "\\n", sep="")
 
         #
-        preg1<-paste(preg, "How much is the sum of all its values which are lower than the mean?")
+        preg1<-paste(preg, "How much is the sum of all the values that are lower than the mean?")
         sol1<-as.character( sum(x[x<mean(x)])*c(1,.75,.5))
         pregunta(preg1, sol1)
 
 	#
-        preg2<-paste(preg, "How much is the sum of all its values which are greater than the mean?")
+        preg2<-paste(preg, "How much is the sum of all the values that are greater than the mean?")
         sol2<-as.character( sum(x[x>mean(x)])*c(1,1.75,1.5))
         pregunta(preg2, sol2)
 
         #
-        preg3<-paste(preg, "How much is the sum of all its values which are lower than the median?")
+        preg3<-paste(preg, "How much is the sum of all the values that are lower than the median?")
         sol3<-as.character( sum(x[x<median(x)])*c(1,.75,.5))
         pregunta(preg3, sol3)
 
 	#
-        preg4<-paste(preg, "How much is the sum of all its values which are greater than the median?")
+        preg4<-paste(preg, "How much is the sum of all the values that are greater than the median?")
         sol4<-as.character( sum(x[x>median(x)])*c(1,.75,1.5))
         pregunta(preg4, sol4)
 
 }
 
 
-pregunta("In order to compute a matrix determinat you would use:",
+pregunta("In order to compute a matrix determinat you will use:",
   c("det(X)", "deter(X)", "determinate(X)")
 )
 
-pregunta("To compute a matrix inverse you would use:",
+pregunta("To compute a matrix inverse you will use:",
    c("solve(X)", "invers(X)", "1/X")
 )
 
-pregunta("To perform a matrix multiplication on two matrices, you would use:",
+pregunta("To perform a matrix multiplication on two matrices, you will use:",
    c("X%*%Y", "X*Y", "XY")
 )
 
-pregunta("To multiply two matrices element-wise, you would use:",
+pregunta("To multiply two matrices element-wise, you will use:",
    c("X*Y", "X%*%Y", "X.Y")
 )
 
-pregunta("To obtain a sample of size 10 without replacement from that vector,
-you would use:",
+pregunta("To obtain a sample of size 10 without replacement from a vector X,
+you will use:",
    c("sample(X,10)", "samples(X,10,TRUE)", "Ninguna de las dos")
 )
 
-pregunta("To obtain a sample of size 3 with replacement from that vector,
-you would use:",
+pregunta("To obtain a sample of size 3 with replacement from a vector X,
+you will use:",
    c("sample(X,3,rep\\=TRUE)", "sample(X,3,FALSE)", "Ninguna de las dos")
 )
 
@@ -178,14 +186,14 @@ for(i in 1:ndistr)
 
 
 	#Probabilidad acumulada
-	preg1<-paste(preg, "In order to compute the probability of X being lower or equal than 3, you would use:", sep="")
+	preg1<-paste(preg, "In order to compute the probability of X being lower or equal than 3, you will use:", sep="")
 	sol1<-paste("p",distr[i], "(3,", paste(params[[i]], collapse= ", "), ")", sep="") 
 	sol1<-c(sol1, paste("1-", sol1,sep=""), "None of the above")
 	
 	pregunta(preg1, sol1)
 
 	#1-Probabilidad acumulada
-	 preg2<-paste(preg, "In order to compute the probability of X being higher than than 3, you would use:", sep="")
+	 preg2<-paste(preg, "In order to compute the probability of X being higher than than 3, you will use:", sep="")
 	sol2<-paste("p",distr[i], "(3,", paste(params[[i]], collapse= ", "), ")", sep="") 
 	sol2<-c(paste("1-", sol2,sep=""), sol2, "None of the above")
 	
@@ -193,7 +201,7 @@ for(i in 1:ndistr)
 
 	#Quantiles
 	qnt<-sample(1:100,1)
-	preg3<-paste(preg, "To compute the ",qnt, "% percentile, you would use:", sep="")
+	preg3<-paste(preg, "To compute the ",qnt, "% percentile, you will use:", sep="")
 	sol3a<-paste("q",distr[i], "(", qnt/100,",",paste(params[[i]], collapse= ", "), ")", sep="") 
 	sol3b<-paste("r",distr[i], "(", qnt/100,",",paste(params[[i]], collapse= ", "), ")", sep="") 
 	sol3c<-paste("p",distr[i], "(", qnt/100,",",paste(params[[i]], collapse= ", "), ")", sep="") 
@@ -201,7 +209,7 @@ for(i in 1:ndistr)
 	pregunta(preg3, c(sol3a, sol3b, sol3c))
 
 	#Simulation
-	preg4<-paste(preg, "To sample 100 values from than random variable, you would use:", sep="")
+	preg4<-paste(preg, "To sample 100 values from than random variable, you will use:", sep="")
 	sol4a<-paste("r",distr[i], "(", 100,",",paste(params[[i]], collapse= ", "), ")", sep="") 
 	sol4b<-paste("q",distr[i], "(", 100,",",paste(params[[i]], collapse= ", "), ")", sep="") 
 	sol4c<-paste("p",distr[i], "(", 100,",",paste(params[[i]], collapse= ", "), ")", sep="") 
@@ -212,17 +220,18 @@ for(i in 1:ndistr)
 
 
 #
-#Practical 3: 
+#Practical 3:  Simulation with R (II)
 #
 
-cat("\n\n$CATEGORY: Ex_Pr3\n\n")
+#cat("\n\n$CATEGORY: Ex_Pr3\n\n")
+GIFTcategory("Ex_Pr3")
 
 
-pregunta("Let if be a factor X. In order to know the number of observations in each level, you would use:",
+pregunta("Let X be a vector of type factor. In order to know the number of observations in each level, you will use:",
    c("table(X)", "levels(X)", "None of the above")
 )
 
-pregunta("In order to create a factor made of letters A, B and C where each letter repeats 10, 20 and 50 times, respectively, you would use:",
+pregunta("In order to create a factor made of letters A, B and C where each letter repeats 10, 20 and 50 times, respectively, you will use:",
   c("factor(rep(c(\"A\",\"B\",\"C\"),c(10,20,30)))", 
     "factor(rep(c(\"A\",\"B\",\"C\"),10,20,50))", 
     "None of the above")
@@ -232,39 +241,39 @@ pregunta("To convert a vector X into a factor:",
    c("as.factor(X)", "fact(X)", "None of the above")
 )
 
-pregunta("To obtain a numeric summary of a data.frame X:",
+pregunta("To obtain a numeric summary of a data.frame X, you will use:",
    c("summary(X)", "Summary(X)", "None of the above")
 )
 
-pregunta("To show the names of the variable in data.frame X:",
+pregunta("To show the names of the variable in data.frame X, you will use:",
    c("names(X)", "variables(X)", "None of the above")
 )
 
-#Questions about data set CochesBig.sav
+#Questions about data set CochesBig.sav (in Spanish)
 library(foreign)
 cochesbig<-read.spss("CochesBig.sav", to.data.frame = TRUE)
 
-variables<-names(cochesbig)#List of all variables names
+variables<-names(cochesbig)#List of all variable names
 marcas<-levels(cochesbig$marca)#Manufaturers/brands
 
 for(v in variables[1:3])
 for(m in marcas[1:5])
 {
 	#Media
-	preg1<-paste("Using the CochesBig.sav data set. The mean of variable ", v, " of manufacturer ", sep="")
+	preg1<-paste("Using the CochesBig.sav data set. The mean of variable ", v, " for manufacturer ", sep="")
 	preg1<-paste(preg1, m, " is", sep="")
 	sol1<-as.character(round(mean(cochesbig[cochesbig$marca==m,v])*c(1, .6, .5), 4))
 	pregunta(preg1, sol1)
 
 
 	#Mediana
-	preg2<-paste("Using the CochesBig.sav data set. The mean of variable ", v, " of manufacturer ", sep="")
+	preg2<-paste("Using the CochesBig.sav data set. The mean of variable ", v, " for manufacturer ", sep="")
 	preg2<-paste(preg2, m, " is", sep="")
 	sol2<-as.character(round(quantile(cochesbig[cochesbig$marca==m, v],.5)*c(1, .6, .5), 4))
 	pregunta(preg2, sol2)
 
 	#Cuantil del 0.6
-	preg3<-paste("Using the CochesBig.sav data set. The 60% percentile of variable ", v, " of manufacturer ", sep="")
+	preg3<-paste("Using the CochesBig.sav data set. The 60% percentile of variable ", v, " for manufacturer ", sep="")
 	preg3<-paste(preg3, m, " is", sep="")
 	sol3<-as.character(round(quantile(cochesbig[cochesbig$marca==m, v],.60)*c(1, .6, .5), 4))
 	pregunta(preg3, sol3)
@@ -277,7 +286,8 @@ for(m in marcas[1:5])
 #
 
 
-cat("\n\n$CATEGORY: Ex_Pr4\n\n")
+#cat("\n\n$CATEGORY: Ex_Pr4\n\n")
+GIFTcategory("Ex_Pr4")
 
 pregunta("The distribution of the mean of 10000 independent and equally distributed random variables...",
    c("... is Normal, according to the Central Limit Theorem.",
@@ -296,7 +306,7 @@ for(m in seq(10, 25, by=5))
 {
 for(ss in seq(30, 50, by=10))
 {
-	preg1<-paste("Given a Normal population with mean ", m, " if I compute 1000 95% confidence intervals with samples of size ", ss, " from that population:", sep="")
+	preg1<-paste("Given a Normal population with mean ", m, ", if I compute 1000 95% confidence intervals with samples of size ", ss, " from that population:", sep="")
 	sol1<-c("None of the above", paste("It is not possible to know how many intervals will contain value ", m, "", sep=""), paste("More than 95% of the intervals will contain value ", m, sep=""))
 	pregunta(preg1, sol1)
 
@@ -333,7 +343,7 @@ for(v in variables[1:3])
 {
 	m0<-round(lm0[[v]]*runif(1, .75,1.25))
 
-	preg1<-paste("Using data set CochesBig.sav. The mean of ", v,
+	preg1<-paste("Using the CochesBig.sav data set. The mean of ", v,
   " can be ", m0, " with a ", conf,"% confidence.", sep="")
 
 	sol1<-c(paste("Yes, becuase that value is inside the ",conf,"% confidence interval", sep=""),
@@ -351,7 +361,7 @@ for(v in variables[1:3])
 
 	}
 
-	preg2<-paste("Using data set CochesBig.sav. A ", conf,   "% confidence interval for variable ", v, "is:", sep="")
+	preg2<-paste("Using the CochesBig.sav data set. A ", conf,   "% confidence interval for variable ", v, "is:", sep="")
  
 	sol2<-c(paste("(", paste(round(as.numeric(tt$conf.int), 4), collapse=", "), ")", sep=""),
    paste("(", paste(5+round(.9*as.numeric(tt$conf.int), 4), collapse=", "), ")", sep=""),
@@ -373,9 +383,9 @@ for(x in seq(5, 10, by=5))
 	p0<-round((x/n)*runif(1,.75,5), 2)
 
 	preg1<-paste("In a factory, ", n, 
-   " pieces were inspected and ", x, " of them were found to be defectuous. ",
+   " pieces were inspected and ", x, " of them were found to be defective. ",
    " With a confidence of ", conf, "%, is there any evidence that",
-   " the proportion of defectuous pieces is equal to ", p0,"?", sep="")
+   " the proportion of defective pieces is equal to ", p0,"?", sep="")
 
 	ttp<-prop.test(x,n, p=p0, conf.level=conf/100)
 
@@ -394,8 +404,8 @@ for(x in seq(5, 10, by=5))
 
 
 	preg2<-paste("In a factory ", n,
-   " pieces were inspected and ", x, " of them were found to be defectuous. ",
-"A ", conf,   "% confidence interval for the proportion of defectuous pieces is", sep="")
+   " pieces were inspected and ", x, " of them were found to be defective. ",
+"A ", conf,   "% confidence interval for the proportion of defective pieces is", sep="")
 
         sol2<-c(paste("(", paste(round(as.numeric(ttp$conf.int), 4), collapse=", "), ")", sep=""),
    paste("(", paste(.15+round(1.1*as.numeric(ttp$conf.int), 4), collapse=", "), ")", sep=""),
@@ -414,8 +424,8 @@ for(x in seq(5, 10, by=5))
 #
 
 
-cat("\n\n$CATEGORY: Ex_Pr5\n\n")
-
+#cat("\n\n$CATEGORY: Ex_Pr5\n\n")
+GIFTcategory("Ex_Pr5")
 
 #Test on the means of two populations
 for(conf in seq(80, 99, by=5))
@@ -438,7 +448,7 @@ for(sigma in seq(5, 5, by=1))
    #Contraste
     preg1<-paste(preg, 
    "Could we say that the mean IQ for boys and girls is the same? ",
-    "Considera un nivel de significación de ", 1-conf/100, ".", sep="")
+    "Consider a significance level of ", 1-conf/100, ".", sep="")
 
 
    sol1<-c("Yes", "No", "We need more data")
@@ -462,22 +472,22 @@ for(sigma in seq(5, 5, by=1))
 
 }
 
-#Contraste de proporciones
+#Test for rates
 for(n1 in seq(190, 200, by=10))
 for(x1 in seq(5, 20, by=5))
 for(n2 in seq(90, 100, by=10))
 for(x2 in seq(5, 10, by=3))
 for( conf in seq(80, 99, by=5))
 {
-	preg<-paste("In order to test two rates two samples ",
-    "are taken from two different populations. Their sample sizes are ", n1, " y ", n2,
+	preg<-paste("In order to test two rates  two samples ",
+    "are taken from two different populations. Their sample sizes are ", n1, " and ", n2,
     ", and the following values were obtained:\\n\\n Number of successes in the first sample: ",
     x1, "\\n\\nNumber of sucesses in the second smaple: ", x2, ".\\n\\n", sep="")
 
 
    #Contraste de medias
    preg1<-paste(preg, 
-  "COuld we say that these two rates are equal with a confidence level of  ",
+  "Could we say that these two rates are equal with a confidence level of  ",
   conf, "%?", sep="") 
 
 	sol1<-c("Yes", "No", "More data are needed.")
@@ -494,8 +504,8 @@ for( conf in seq(80, 99, by=5))
 
 
    #p-valor
-   preg2<-paste(preg, "The p-value associated to the test is:", sep="")
-   sol2<-c(as.character(ttp$p.value), as.character(1-ttp$p.value), "Ninguno de los dos")
+   preg2<-paste(preg, "The p-value associated with the test is:", sep="")
+   sol2<-c(as.character(ttp$p.value), as.character(1-ttp$p.value), "None of the above")
    pregunta(preg2, sol2)
 
 }
@@ -506,7 +516,9 @@ for( conf in seq(80, 99, by=5))
 #
 
 #ANOVA
-cat("\n\n$CATEGORY: Ex_Pr6_AOV\n\n")
+#cat("\n\n$CATEGORY: Ex_Pr6_AOV\n\n")
+GIFTcategory("Ex_PR6_AOV")
+
 
 for(media1 in seq(95, 100, by=5))
 for(media2 in seq(95, 100, by=5))
@@ -526,12 +538,12 @@ catalizador<-data.frame(catalizador=rep(c("C1","C2","C3"), each=3),
 aovcat<-aov(produccion~catalizador, data=catalizador)
 saovcat<-summary(aovcat)
 
-preg<-"An experiment was made to measure the production realted to three different catalyzers used in an industrial process. The experiments was repeated three times for each catalyzer. Productions in grams were:\\n\\n"
+preg<-"An experiment was made to measure the production related to three different catalyzers used in an industrial process. The experiment was repeated three times for each catalyzer. Productions in grams were:\\n\\n"
 
 
-	preg<-paste(preg, "Catalizador 1:", paste(x1, collapse=","), "\\n\\n")
-	preg<-paste(preg, "Catalizador 2:", paste(x2, collapse=","), "\\n\\n")
-	preg<-paste(preg, "Catalizador 3:", paste(x3, collapse=","), "\\n\\n")
+	preg<-paste(preg, "Catalyzer 1:", paste(x1, collapse=","), "\\n\\n")
+	preg<-paste(preg, "Catalyzer 2:", paste(x2, collapse=","), "\\n\\n")
+	preg<-paste(preg, "Catalyzer 3:", paste(x3, collapse=","), "\\n\\n")
 
 
 	#CM ENTRE
@@ -562,7 +574,9 @@ preg<-"An experiment was made to measure the production realted to three differe
 
 
 #Linear regression
-cat("\n\n$CATEGORY: Ex_Pr6_RL\n\n")
+#cat("\n\n$CATEGORY: Ex_Pr6_RL\n\n")
+GIFTcategory("Ex_Pr6_LR")
+
 
 lpred<-list(pvp=10000, emision=200, potencia=150, costecar=800, nplaza=3)
 
@@ -576,20 +590,18 @@ preg<-paste("Using the cochesBig.sav data set, consider the linear model where v
 	m1<-lm(f1, data=cochesbig)
 	m2<-lm(f2, data=cochesbig)
 
-	#Interceptación
-	preg1<-paste(preg, "What is the independent term in the model?", sep="")
+	#Intercept
+	preg1<-paste(preg, "What is the value of the intercept of the model?", sep="")
 sol1<-as.character(round(c(coef(m1)[[1]], coef(m1)[[2]], coef(m2)[[1]]), 4))
 	pregunta(preg1, sol1)
 
-
-
-	#Coeficiente
-	preg2<-paste(preg, "What's the value of the regression coefficient of the predictive variable in the model?", sep="")
+	#Coefficient 
+	preg2<-paste(preg, "What's the value of the regression coefficient of the predictive variable of the model?", sep="")
 sol2<-as.character(round(c(coef(m1)[[2]], coef(m1)[[1]], coef(m2)[[2]]), 4))
 	pregunta(preg2, sol2)
 
 
-	#Predicción
+	#Prediction
 	preg3<-paste(preg, "What is the value of 'consumo' for a car with a value of ",  
         v, " of ", lpred[v][[1]],"?", sep="")
 	sol3<-as.character(round(c(predict(m1, as.data.frame(lpred[v]))[[1]],
@@ -598,7 +610,7 @@ sol2<-as.character(round(c(coef(m1)[[2]], coef(m1)[[1]], coef(m2)[[2]]), 4))
 	pregunta(preg3, sol3)
 }
 
-#FIN
+#THE END
 sink()
 
 
@@ -606,7 +618,4 @@ sink()
 #system("iconv -f ISO-8859-1 -t UTF8 test_examen.txt>kk.txt")
 #system("cp kk.txt test_examen.txt")
 #system("rm kk.txt")
-
-
-
 
