e50d376dd33c56c06243662ca200b07e590ff43a
virgil <virgil@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-31 00:50:40 +0000

    Added local encoding.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rgift@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/doc/UTF8.R
+++ pkg/inst/doc/UTF8.R
@@ -1,9 +1,11 @@
 #Example of use of text in UTF-8 encoding
 library(RGIFT)
 
+
+Sys.setlocale(locale="es_ES.iso-8859-15")
 txt<-"¿Es Madrid la capital de España?"
 
-#Local enconding:
+#Get Local enconding:
 #Option 1: Set locale
 #locale<-"es_ES.iso-8859-15"
 #Option 2: Get locale from OS
